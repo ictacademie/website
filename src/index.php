8b93717241a181906c35ce855e7f18b7be96f623
William Willing <william@williamwilling.com>
2014-03-02 22:30:33 +0100

    Improved the styling of the table of contents and added dummy images.

--- src/index.php
+++ src/index.php
@@ -18,56 +18,111 @@
         <nav class="toc">
           <ul>
             <li>
-              <p><a href=".">Applicatieontwikkelaar</a> <span>niveau 4, bol</span></p>              
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/applicatieontwikkelaar/">Applicatieontwikkelaar</a> <span>niveau 4, bol</span></p>              
+              <img src="applicatieontwikkelaar.jpg">
               <p>Iets over de opleiding Applicatieontwikkelaar.</p>
             </li>
 
             <li>
-              <p>ICT-beheerder <span>niveau 4, bol</span></p>
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/ict-beheerder/">ICT-beheerder</a> <span>niveau 4, bol</span></p>
+              <img src="ict-beheerder.jpg">
               <p>Iets over de opleiding ICT-beheerder.</p>
             </li>
 
             <li>
-              <p>Netwerkbeheerder <span>niveau 4, bol en bbl</span></p>
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/netwerkbeheerder/">Netwerkbeheerder</a> <span>niveau 4, bol en bbl</span></p>
+              <img src="netwerkbeheerder.jpg">
               <p>Iets over de opleiding Netwerkbeheerder.</p>
             </li>
 
             <li>
-              <p>Medewerker beheer ICT <span>niveau 3, bol en bbl</span></p>
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/medewerker-beheer-ict/">Medewerker beheer ICT</a> <span>niveau 3, bol en bbl</span></p>
+              <img src="medewerker-beheer-ict.jpg">
               <p>Iets over de opleiding Medewerker beheer ICT.</p>
             </li>
 
             <li>
-              <p>Telecommonteur <span>niveau 3, bbl</span></p>
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/telecommonteur/">Telecommonteur</a> <span>niveau 3, bbl</span></p>
+              <img src="telecommonteur.jpg">
               <p>Iets over de opleiding Telecommonteur.</p>
             </li>
 
             <li>
-              <p>Medewerker ICT <span>niveau 2, bol</span></p>
-              <img src="." width="128" height="72">
+              <p><a href="/opleidingen/medewerker-ict/">Medewerker ICT </a><span>niveau 2, bol</span></p>
+              <img src="medewerker-ict.jpg">
               <p>Iets over de opleiding Medewerker ICT.</p>
             </li>
           </ul>
         </nav>
 
+        <h2>Leerbedrijven</h2>
+
+        <nav class="toc">
+          <ul>
+            <li>
+              <p><a href="/leerbedrijven/gates-and-jobs/">Gates &amp; Jobs</a></p>
+              <img src="gates-and-jobs.jpg">
+              <p>Maak websites enzo voor klanten. Gates & Jobs is zo cool.</p>
+            </li>
+
+            <li>
+              <p><a href="/leerbedrijven/lisa/">Lisa</a></p>
+              <img src="lisa.jpg">
+              <p>Programmeren dingen voor het onderwijs. Lisa is zo awesome.</p>
+            </li>
+
+            <li>
+              <p><a href="/leerbedrijven/refurbizz/">Refurbizz</a></p>
+              <p>Sloop computers en verdien er geld mee. Refurbizz is zo rad.</p>
+              <img src="refurbizz.jpg">
+            </li>
+
+            <!-- This list item only exists to make the columns equal height. -->
+            <li></li>
+          </ul>
+        </nav>
+
         <h2>Locaties</h2>
 
-        <ul>
-          <li>
-            <p>Dordrecht</p>
-          </li>
+        <nav class="toc">
+          <ul>
+            <li>
+              <p><a href="/locaties/dordrecht/">Dordrecht</a></p>
+              <img src="dordrecht.jpg">
+            </li>
 
-          <li>
-            <p>Gorinchem</p>
-          </li>
-        </ul>
+            <li>
+              <p><a href="/locaties/gorinchem/">Gorinchem</a></p>
+              <img src="gorinchem.jpg">
+            </li>
+          </ul>
+        </nav>
 
         <h2>Vakkennis</h2>
+
+        <nav class="toc">
+          <ul>
+            <li>
+              <p><a href="/vakkennis/c-sharp/">C#</a></p>
+              <img src="c-sharp.jpg">
+            </li>
+
+            <li>
+              <p><a href="/vakkennis/git/">Git</a></p>
+              <img src="git.jpg">
+            </li>
+
+            <li>
+              <p><a href="/vakkennis/git/">Linux</a></p>
+              <img src="linux.jpg">
+            </li>
+
+            <li>
+              <p><a href="/vakkennis/c-sharp/">Lua</a></p>
+              <img src="lua.jpg">
+            </li>
+          </ul>
+        </nav>
       </div>
     </article>
 
