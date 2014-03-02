cef89b6bcd2b0849ddc8c7518ba3b26000db5c40
William Willing <william@williamwilling.com>
2014-03-02 12:49:14 +0100

    Added some more outline content, for courses and locations.

--- src/index.php
+++ src/index.php
@@ -9,8 +9,65 @@
 
       <div>
         <h2>Opleidingen</h2>
+        <ul>
+          <li>
+            <p>Applicatieontwikkelaar</p>
+            <p>niveau 4, bol</p>
+            <p>Iets over de opleiding Applicatieontwikkelaar.</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>ICT-beheerder</p>
+            <p>niveau 4, bol</p>
+            <p>Iets over de opleiding ICT-beheerder.</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>Netwerkbeheerder</p>
+            <p>niveau 4, bol en bbl</p>
+            <p>Iets over de opleiding Netwerkbeheerder.</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>Medewerker beheer ICT</p>
+            <p>niveau 3, bol en bbl</p>
+            <p>Iets over de opleiding Medewerker beheer ICT.</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>Telecommonteur</p>
+            <p>niveau 3, bbl</p>
+            <p>Iets over de opleiding Telecommonteur.</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>Medewerker ICT</p>
+            <p>niveau 2, bol</p>
+            <p>Iets over de opleiding Medewerker ICT.</p>
+          </li>
+        </ul>
 
         <h2>Locaties</h2>
+
+        <ul>
+          <li>
+            <p>Dordrecht</p>
+          </li>
+
+          <li>
+            <p>Gorinchem</p>
+          </li>
+        </ul>
 
         <h2>Vakkennis</h2>
       </div>
