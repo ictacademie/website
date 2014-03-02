0f31a996ba70a22cb9320cdb9e2b36c6a5f10b2d
William Willing <william@williamwilling.com>
2014-03-02 12:34:17 +0100

    Created the outline for the main page.

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <body>
+    <article>
+      <header>
+        <h1>ICT Academie</h1>
+        <p>De ICT Academie is een kleine, gezellige school voor ICT met een persoonlijke aanpak. Wij geloven dat je het best leert als je jezelf kunt zijn en je eigen interesses kunt volgen.</p>
+      </header>
+
+      <div>
+        <h2>Opleidingen</h2>
+
+        <h2>Locaties</h2>
+
+        <h2>Vakkennis</h2>
+      </div>
+    </article>
+
+    <footer>
+      <p>De ICT Academie is onderdeel van het <a href="http://www.davinci.nl/">Da Vinci College.</a><p>
+    </footer>
+  </body>
+</html>
